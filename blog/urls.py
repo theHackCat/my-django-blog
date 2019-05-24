71633b85dafefe9121fdc89bd71b8011d5e25af7
thehackcat <kgruca21@gmail.com>
2019-05-23 19:20:05 -0700

    added user registration

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # path('accounts/login/', views.user_login, name='login'),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
--- blog/views.py
+++ blog/views.py
@@ -7,9 +7,26 @@ from .forms import PostForm, CommentForm
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(
+            user_form.cleaned_data['password'])
+            # Save the User object
+            new_user.save()
+            return render(request, 'blog/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'blog/register.html', {'user_form': user_form})
+
 
 def user_login(request):
     if request.method == 'POST':
