346d99ab9f9c3d54c2400ee80007ff15d4b42c03
thehackcat <kgruca21@gmail.com>
2019-05-23 18:07:24 -0700

    tests again

--- blog/forms.py
+++ blog/forms.py
@@ -12,3 +12,7 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('author', 'text')
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
