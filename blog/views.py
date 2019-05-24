6562becf5cc33b1ab57fcf3c89def155c74a0cfa
thehackcat <kgruca21@gmail.com>
2019-05-23 18:42:23 -0700

    added diff login view for test

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,30 @@ from django.utils import timezone
 from .forms import PostForm
 from django.contrib.auth.decorators import login_required
 from .forms import PostForm, CommentForm
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
 # Create your views here.
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'blog/login.html', {'form': form})
 
 @login_required
 def post_list(request):
